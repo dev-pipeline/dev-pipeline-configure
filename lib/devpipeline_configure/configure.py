ecb0fd8700e8fd213efff2603666ca270e8a9282
Stephen Newell <sjnewell@gmail.com>
2020-05-16 23:35:42 -0600

    Migrate to the newer subcommand APIs

--- lib/devpipeline_configure/configure.py
+++ lib/devpipeline_configure/configure.py
@@ -14,62 +14,49 @@ def _choose_build_dir(arguments):
     return arguments.build_dir_basename
 
 
-class Configure(devpipeline_core.command.Command):
-
-    """This class manages the configuration of the project."""
-
-    def __init__(self):
-        super().__init__(
-            prog="dev-pipeline configure", description="Configure a project"
-        )
-        self.add_argument(
-            "--config", help="Build configuration file", default="build.config"
-        )
-        self.add_argument(
-            "--profile",
-            help="Build-specific profiles to use.  If more than one profile is required, separate their names with commas.",
-        )
-        self.add_argument(
-            "--override",
-            help="Collection of override options to use.  If you require multiple types of overrides, separate the names with commas.",
-        )
-        self.add_argument(
-            "--build-dir",
-            help="Directory to store configuration.  If specified, --build-dir-basename will be ignored.",
-        )
-        self.add_argument(
-            "--build-dir-basename",
-            help="Basename for build directory configuration",
-            default="build",
-        )
-        self.add_argument(
-            "--root-dir",
-            help="Root directory for checkouts.  Defaults to the same path as build configuration.",
-        )
-        self.set_version(devpipeline_configure.version.STRING)
-
-    def process(self, arguments):
-        ex_args = {
-            "profiles": arguments.profile,
-            "overrides": arguments.override,
-        }
-        if "root_dir" in arguments:
-            ex_args["src_root"] = arguments.root_dir
-        config = devpipeline_configure.config.process_config(
-            arguments.config, _choose_build_dir(arguments), "build.cache", **ex_args
-        )
-        devpipeline_core.sanitizer.sanitize(
-            config, lambda n, m: print("{} [{}]".format(m, n))
-        )
+def _configure(parser):
+    parser.add_argument(
+        "--config", help="Build configuration file", default="build.config"
+    )
+    parser.add_argument(
+        "--profile",
+        help="Build-specific profiles to use.  If more than one profile is required, separate their names with commas.",
+    )
+    parser.add_argument(
+        "--override",
+        help="Collection of override options to use.  If you require multiple types of overrides, separate the names with commas.",
+    )
+    parser.add_argument(
+        "--build-dir",
+        help="Directory to store configuration.  If specified, --build-dir-basename will be ignored.",
+    )
+    parser.add_argument(
+        "--build-dir-basename",
+        help="Basename for build directory configuration",
+        default="build",
+    )
+    parser.add_argument(
+        "--root-dir",
+        help="Root directory for checkouts.  Defaults to the same path as build configuration.",
+    )
+    devpipeline_core.command.add_version_info(
+        parser, devpipeline_configure.version.STRING
+    )
 
 
-def main(args=None):
-    # pylint: disable=missing-docstring
-    configure = Configure()
-    devpipeline_core.command.execute_command(configure, args)
+def _execute(arguments):
+    ex_args = {
+        "profiles": arguments.profile,
+        "overrides": arguments.override,
+    }
+    if "root_dir" in arguments:
+        ex_args["src_root"] = arguments.root_dir
+    config = devpipeline_configure.config.process_config(
+        arguments.config, _choose_build_dir(arguments), "build.cache", **ex_args
+    )
+    devpipeline_core.sanitizer.sanitize(
+        config, lambda n, m: print("{} [{}]".format(m, n))
+    )
 
 
-_CONFIGURE_COMMAND = (main, "Configure a project bulid directory.")
-
-if __name__ == "__main__":
-    main()
+_CONFIGURE_COMMAND = ("Configure a project build directory.", _configure, _execute)
--- lib/devpipeline_configure/reconfigure.py
+++ lib/devpipeline_configure/reconfigure.py
@@ -29,63 +29,44 @@ def _calculate_value(config, config_key, arguments, key):
     return original
 
 
-class Reconfigure(devpipeline_core.command.Command):
-
-    """This class reconfigures an existing project."""
-
-    def __init__(self):
-        super().__init__(
-            prog="dev-pipeline reconfigure",
-            description="Reconfigure a project, optionally with additional "
-            "parameters",
-        )
-        self.add_argument(
-            "--profiles",
-            help="Comma-separated list of profiles to use.",
-            default=argparse.SUPPRESS,
-        )
-        self.add_argument(
-            "--overrides",
-            help="Comma-separated list of overrides to use.",
-            default=argparse.SUPPRESS,
-        )
-        self.add_argument(
-            "--append",
-            help="Append provided profiles/overrides instead of replacing",
-            action="store_true",
-        )
-        self.set_version(devpipeline_configure.version.STRING)
-
-    def process(self, arguments):
-        config_file = devpipeline_configure.load.find_config()
-        cache_config = devpipeline_configure.parser.read_config(config_file)
-        profiles = _calculate_value(
-            cache_config, "dp.profile_name", arguments, "profiles"
-        )
-        overrides = _calculate_value(
-            cache_config, "dp.overrides", arguments, "overrides"
-        )
-
-        cache = devpipeline_configure.config.process_config(
-            cache_config.get("DEFAULT", "dp.build_config"),
-            os.path.dirname(config_file),
-            "build.cache",
-            profiles=profiles or None,
-            overrides=overrides or None,
-            src_root=cache_config.get("DEFAULT", "dp.src_root"),
-        )
-        devpipeline_core.sanitizer.sanitize(
-            cache, lambda n, m: print("{} [{}]".format(m, n))
-        )
+def _configure(parser):
+    parser.add_argument(
+        "--profiles",
+        help="Comma-separated list of profiles to use.",
+        default=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--overrides",
+        help="Comma-separated list of overrides to use.",
+        default=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--append",
+        help="Append provided profiles/overrides instead of replacing",
+        action="store_true",
+    )
+    devpipeline_core.command.add_version_info(
+        parser, devpipeline_configure.version.STRING
+    )
 
 
-def main(args=None):
-    # pylint: disable=missing-docstring
-    reconfigure = Reconfigure()
-    devpipeline_core.command.execute_command(reconfigure, args)
+def _execute(arguments):
+    config_file = devpipeline_configure.load.find_config()
+    cache_config = devpipeline_configure.parser.read_config(config_file)
+    profiles = _calculate_value(cache_config, "dp.profile_name", arguments, "profiles")
+    overrides = _calculate_value(cache_config, "dp.overrides", arguments, "overrides")
+
+    cache = devpipeline_configure.config.process_config(
+        cache_config.get("DEFAULT", "dp.build_config"),
+        os.path.dirname(config_file),
+        "build.cache",
+        profiles=profiles or None,
+        overrides=overrides or None,
+        src_root=cache_config.get("DEFAULT", "dp.src_root"),
+    )
+    devpipeline_core.sanitizer.sanitize(
+        cache, lambda n, m: print("{} [{}]".format(m, n))
+    )
 
 
-_RECONFIGURE_COMMAND = (main, "Reconfigure a project bulid directory.")
-
-if __name__ == "__main__":
-    main()
+_RECONFIGURE_COMMAND = ("Reconfigure a project build directory.", _configure, _execute)
